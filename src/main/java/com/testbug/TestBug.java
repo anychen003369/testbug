8200faaa4b89626d14a4abcc7da0a43827376d18
anychen <422322890@qq.com>
2018-05-18 12:14:05 +0800

    提交

--- src/main/java/com/testbug/TestBug.java
+++ src/main/java/com/testbug/TestBug.java
@@ -4,5 +4,6 @@ public class TestBug {
     public static void main(String[] args) {
         System.out.println("this is test");
         System.out.println("this is test2");
+        System.out.println("this is test3");
     }
 }
