06b8a68977ac74bf16a5c7c1844dab17180217fa
anychen <422322890@qq.com>
2018-05-18 12:00:17 +0800

    同步

--- src/main/java/com/testbug/TestBug.java
+++ src/main/java/com/testbug/TestBug.java
@@ -3,5 +3,6 @@ package com.testbug;
 public class TestBug {
     public static void main(String[] args) {
         System.out.println("this is test");
+        System.out.println("this is test2");
     }
 }
